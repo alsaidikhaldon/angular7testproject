4dcbd6394e45ffaa85ffce96e34ab8932d4d3ffd
unknown <alsaidi.khaldon@gmail.com>
2019-01-10 05:13:03 +0100

    second change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeaderComponent, FooterComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, FooterComponent, ContentComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
